ce9f39fc53453a73422e644f1c7f6ebb41aa307f
Tyler Battle <tyler@battle.ca>
2021-07-06 19:43:46 -0700

    Initial run of rename script

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,8 +1,8 @@
 import unittest
 
-import stactools.package
+import stactools.ga_nlcd
 
 
 class TestModule(unittest.TestCase):
     def test_version(self):
-        self.assertIsNotNone(stactools.package.__version__)
+        self.assertIsNotNone(stactools.ga_nlcd.__version__)
